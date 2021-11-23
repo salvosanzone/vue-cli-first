c15ddf4630d5579ad7e2aa593e6f7da4f710d2b7
salvosanzone <salvosansone.ss@gmail.com>
2021-11-23 16:43:46 +0100

    caricamento file Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="container">
+    <h1>
+      La mia prima app con Vue CLI!
+    </h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    ciao
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Main'
+}
+</script>
+
+<style>
+
+</style>
